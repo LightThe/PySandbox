29b1bc2eafc6a96196c47ccb60a1c779b86e6b23
Théo Gallo <gallotheo@icloud.com>
2019-04-01 09:14:14 -0300

    spp06 completo, readme atualizado?

--- SPP/ex06.py
+++ SPP/ex06.py
@@ -1,10 +1,10 @@
 #Write a program that asks the user for a number n and gives them the possibility to choose between computing the sum and computing the product of 1,…,n.
 
 num=eval(input("Insert a number"))
-opt=eval(input("Insert 1 if you want to multiply, 2 if you want to sum"))
+opt=eval(input("Type 1 if you want to multiply, 2 if you want to sum"))
 if opt==1:
 	res=1
-	for x in range(1, num):
+	for x in range(1, num+1):
 		res*=x
 elif opt==2:
 	res=0
